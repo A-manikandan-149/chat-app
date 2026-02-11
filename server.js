16327178e8579d84c0ed965ce01962d5ff14a7f3
A-manikandan-149 <mk09200401@gmail.com>
2026-02-11 23:46:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve static files from public folder
+// Serve frontend
 app.use(express.static(path.join(__dirname, "public")));
 
-// Route for home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-// Socket connection
+// Chat logic
 io.on("connection", (socket) => {
   console.log("User connected");
 
@@ -28,7 +27,6 @@ io.on("connection", (socket) => {
   });
 });
 
-// Start server
 server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
